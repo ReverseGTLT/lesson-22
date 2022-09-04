e9f0711df7c98ce273f1a5ddbdbdac7a2f579728
ReverseGTLT <dzewrpifzc@gmail.com>
2022-09-04 21:48:07 +0300

    headermob

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,24 @@
-$(document).ready(function(){
-    $('.header-navbar__hamburger').on('click', function() {
-      $(this).toggleClass('active');
-      $('.header-navbar__list').toggleClass('active');
-    });
-  });
+let mobile = document.querySelector('#mobile');
+let button = document.querySelector('#button');
+let navList = document.querySelector('#navList');
+let mobileClose = document.querySelector('#mobileClose')
+let blur = document.querySelector('#blur')
+
+
+button.addEventListener('click', function () {
+    //mobile.classList.remove('header-navbar__list');
+    blur.classList.add('blur');
+    mobile.classList.add('mobile-open');
+    navList.classList.add('mobile-vision');
+    mobileClose.classList.add('vision');
+
+})
+
+mobileClose.addEventListener('click', function () {
+    //mobile.classList.remove('header-navbar__list');
+    mobile.classList.remove('mobile-open');
+    navList.classList.remove('mobile-vision');
+    mobileClose.classList.remove('vision');
+    blur.classList.remove('blur');
+})
+
